8adc6370f83bc8fc4533ed9f57999c024662baa4
Daniel Słaby <pufek93@gmail.com>
2017-03-15 17:38:52 +0100

    wjoijewfq

--- app/src/main/java/com/example/slaby/android_2/MainActivity.java
+++ app/src/main/java/com/example/slaby/android_2/MainActivity.java
@@ -7,10 +7,12 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    static public boolean isAfterChange = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,34 +21,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onStartClick(View view) {
-        // Do something in response to button
-        TextView helloText = (TextView) findViewById(R.id.helloText);
-        EditText input = (EditText) findViewById(R.id.editText);
 
-        String finalText = (String) getResources().getText(R.string.Hello);
-        if (input.getText().length() > 0) {
-            finalText += " " + input.getText();
+        if (!isAfterChange) {
+            // Do something in response to button
+            TextView helloText = (TextView) findViewById(R.id.helloText);
+            EditText input = (EditText) findViewById(R.id.editText);
+
+            String finalText = (String) getResources().getText(R.string.Hello);
+            if (input.getText().length() > 0) {
+                finalText += " " + input.getText();
+            } else {
+                finalText += " " + getResources().getText(R.string.Stranger);
+            }
+
+            helloText.setText(finalText);
+            input.setFreezesText(true);
+            input.setEnabled(false);
+
+            isAfterChange = true;
+            Button btn = (Button) findViewById(R.id.button);
+            btn.setText(getResources().getText(R.string.change_color_button));
         } else {
-            finalText += " " + getResources().getText(R.string.Stranger);
+            TextView helloText = (TextView) findViewById(R.id.helloText);
+
+            if (helloText.getCurrentTextColor() == ContextCompat.getColor(this, R.color.GREEN)) {
+                helloText.setTextColor(ContextCompat.getColor(this, R.color.RED));
+            } else if (helloText.getCurrentTextColor() == ContextCompat.getColor(this, R.color.RED)) {
+                helloText.setTextColor(ContextCompat.getColor(this, R.color.BLUE));
+            } else {
+                helloText.setTextColor(ContextCompat.getColor(this, R.color.GREEN));
+            }
         }
 
-        helloText.setText(finalText);
-        input.setFreezesText(true);
-        input.setEnabled(false);
-        findViewById(R.id.button).setVisibility(View.INVISIBLE);
-        findViewById(R.id.changeColorButton).setVisibility(View.VISIBLE);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public void onChangeColorClick(View view) {
-        TextView helloText = (TextView) findViewById(R.id.helloText);
-
-        if (helloText.getCurrentTextColor() == ContextCompat.getColor(this, R.color.GREEN)) {
-            helloText.setTextColor(ContextCompat.getColor(this, R.color.RED));
-        } else if (helloText.getCurrentTextColor() == ContextCompat.getColor(this, R.color.RED)) {
-            helloText.setTextColor(ContextCompat.getColor(this, R.color.BLUE));
-        } else {
-            helloText.setTextColor(ContextCompat.getColor(this, R.color.GREEN));
-        }
     }
 }
